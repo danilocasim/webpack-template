10ac8dee5fea1e341c143a40a0a96f98f7ad836e
Danilo Casim Jr. <danilocasim174@gmail.com>
2025-02-12 12:27:02 +0800

    Fix: type from commonJS to module in package json and add initial folder and file

--- src/index.js
+++
@@ -1,1 +0,0 @@
-alert("Hello, My love");
--- webpack.common.js
+++ webpack.common.js
@@ -2,13 +2,13 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: "./src/js/index.js",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
   },
-  plugins: [new HtmlWebpackPlugin({ template: "./src/index.html" })],
+  plugins: [new HtmlWebpackPlugin({ template: "./src/template/index.html" })],
   module: {
     rules: [
       {
